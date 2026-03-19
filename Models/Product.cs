811b893e8431e4ff9a13e4293640fbc3acbba6a7
Ömer Apaydın <omerapaydin90@gmail.com>
2026-03-20 02:25:58 +0300

    Models

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
